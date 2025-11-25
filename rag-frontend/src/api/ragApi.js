ed8a63764debf337aa3303e390d453c5bb539a61
EvanMcCormick37 <e.kidmccorm@gmail.com>
2025-11-25 14:29:20 -0700

    Vercel Proxy enabled for HTTP calls
    -Reconfigured the BASE_URL and added Vercel.json to allow for Vercel Proxy API calls. This allows me to make HTTP requests to the back-end from Vercel app.

--- rag-frontend/src/api/ragApi.js
+++ rag-frontend/src/api/ragApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = ''; //Replace with VITE_BASE_URL for dev mode
 const API_PREFIX = import.meta.env.VITE_API_PREFIX;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
