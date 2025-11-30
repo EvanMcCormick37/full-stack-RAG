e638ff789574ca75018d8cf6b07cef10973ab700
EvanMcCormick37 <e.kidmccorm@gmail.com>
2025-11-30 16:41:28 -0700

    Implementing Prompt-rounting classes in prompts.py

--- rag-backend/app/core/prompts.py
+++ rag-backend/app/core/prompts.py
@@ -1,10 +1,38 @@
+from enum import Enum
+
+class PromptStyle(str, Enum):
+    """Enum class capturing final prompt styles."""
+    DISTRACT = "UNRELATED"
+    ANALOGIZE = "ANALOGIZE"
+    ANSWER = "ANSWER"
+
+
 PROMPT_STYLES = {
-            "distract": """
+            PromptStyle.DISTRACT: """
             You are a yapper.
             Acknowledge and explore the user's question, then find a way to relate it to the information contained in the context documents, which you find endlessly fascinating.
             Do not cite the context or mention that you are referring to documents. These are your own ideas that you're excited to share.
             \n\nQuestion: {question} \n\nContext:\n{context}.
             """,
-            "analogize":"Answer the users question using the provided context. Question: {question}\nContext: {context}",
-            "answer": "You are a knowledgeable professor, and an expert in the field. Answer the student's question in detail, using the context documents provided to augment your existing knowledge and expertise. Cite the sources you use when appropriate. \n\nContext:\n{context},\n\nQuestion: {question}",
+            PromptStyle.ANALOGIZE:"""
+            Answer the user's question by way of analogy, using the provided context. Do not cite the context documents, but feel free to elaborate on them. They are your own ideas which you find endlessly fascinating.
+            Question: {question}\nContext: {context}
+            """,
+            PromptStyle.ANSWER: """You are a knowledgeable professor, and an expert in the field.
+            Answer the student's question in detail, using the context documents provided to augment your existing knowledge and expertise.
+            Cite the sources you use when appropriate. \n\nContext:\n{context},\n\nQuestion: {question}""",
         }
+
+ROUTING_PROMPT = """
+Determine the best response strategy for the following question and context documents.
+
+Question: {question},
+Context: {context},
+
+Choose ONE response strategy:
+- ANSWER: If the question can be answered to a reasonable degree using given the context.
+- ANALOGIZE: If the question can be answered by using analogies drawn from the given context.
+- UNRELATED: If the question cannot be answered or analogized using the given context.
+
+ONLY provide the chosen response strategy, as a one-word, all-caps answer. Do not provide any additional text.
+"""
